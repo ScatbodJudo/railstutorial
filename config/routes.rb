ce74aaaa44ce73ab00ffed68d5c74784d2860d61
Todd Jacobus <rtjacobus@alaska.edu>
2016-04-28 18:43:46 -0600

    add title thingy to layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :posts
   resources :users
-  root 'application#hello'
+  root 'static_pages#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
